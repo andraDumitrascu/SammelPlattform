3713d65d5f09b7d8f63047414bb67063fed75b1c
andraDumitrascu <a.dumitrascu@htlwy.at>
2025-04-02 12:44:09 +0200

    Added toggle for darkmode and light mode

--- Log-In/Log-In.js
+++ Log-In/Log-In.js
@@ -1,1 +1,20 @@
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+    
+    if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('darkMode', 'enabled');
+        document.body.classList.remove('light-mode');
+    } else {
+        localStorage.setItem('darkMode', 'disabled');
+        document.body.classList.add('light-mode');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('darkMode') === 'enabled') {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.add('light-mode');
+    }
+});
 
