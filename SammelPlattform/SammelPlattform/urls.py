d0c0b5114dad893c9c48dacbb8283b1ee078ec78
andraDumitrascu <a.dumitrascu@htlwy.at>
2025-04-30 20:16:16 +0200

    changed layout so that it works with django

---
+++ SammelPlattform/SammelPlattform/urls.py
@@ -0,0 +1,27 @@
+"""
+URL configuration for SammelPlattform project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from SammelPlatt import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('kontakt/', views.kontakt, name='kontakt'),
+    path('login/', views.login_view, name='login'),
+    path('datenschutz/', views.datenschutz, name='datenschutz'),
+    path('terms/', views.terms, name='terms'),
+    path('reviews/', views.reviews, name='reviews'),
+]
